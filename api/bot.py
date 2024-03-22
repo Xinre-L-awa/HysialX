97bec0e306e1f8b1c4b92f379d8e38a14854b02a
Xinre_L_awa <75836010+Xinre-L-awa@users.noreply.github.com>
2024-03-22 08:28:23 +0800

    使用 websockets 代替 httpx 调用api

--- api/bot.py
+++ api/bot.py
@@ -1,4 +1,7 @@
+import json
 import httpx
+import asyncio
+import websockets
 
 from log import logger
 from .utils import CreateOnWaitingTask
@@ -7,12 +10,22 @@ from plugins.manager import WaitingFuncMeta
 
 async def send_(uid, mode, message):
     try:
-        async with httpx.AsyncClient(base_url="http://127.0.0.1:570") as client:
-            params = {
-                f"{mode}_id": uid,
-                "message": message,
-            }
-            await client.post("/send_msg", params=params)
+        # async with httpx.AsyncClient(base_url="http://127.0.0.1:570") as client:
+        #     params = {
+        #         f"{mode}_id": uid,
+        #         "message": message,
+        #     }
+        #     await client.post("/send_msg", params=params)
+        
+        async def send(uid, mode, message):
+            async with websockets.connect("http://127.0.0.1:1696/event") as websocket:
+                await websocket.send(
+                    {
+                        f"{mode}_id": uid,
+                        "message": message,
+                    }
+                )
+        await send(uid, mode, message)
         logger.opt(colors=True).success(f'发送消息 "{message}" 到群 {uid} 成功')
     except Exception as e:
         logger.warning(e)
@@ -28,15 +41,25 @@ class Bot:
 
     async def send(self, uid, message, mode="group"):
         try:
-            async with httpx.AsyncClient(base_url="http://127.0.0.1:570") as client:
-                params = {
-                    f"{mode}_id": uid,
-                    "message": message,
-                }
-                await client.post("/send_msg", params=params)
+            # async with httpx.AsyncClient(base_url="http://127.0.0.1:570") as client:
+            #     params = {
+            #         f"{mode}_id": uid,
+            #         "message": message,
+            #     }
+            #     await client.post("/send_msg", params=params)
+            async with websockets.connect("ws://127.0.0.1:1696/event/") as websocket:
+                await websocket.send(
+                        json.dumps({
+                            "action": "send_msg",
+                            "params": {
+                                f"{mode}_id": uid,
+                                "message": message
+                            }
+                        })
+                )
             logger.opt(colors=True).success(f'发送消息 "{message}" 到群 {uid} 成功')
         except Exception as e:
-            logger.warning(e)
+            logger.exception(e)
             logger.opt(colors=True).warning(f'发送消息 "{message}" 到群 {uid} 失败, 账号可能被风控')
         # return await asyncio.gather(send_(uid, "group", message), return_exceptions=True)
     
@@ -49,6 +72,6 @@ class Bot:
             await client.post(f"/{api}", params=params)
         logger.opt(colors=True).success(f"Succeeded to call api {api}!")
     
-    async def input_value(self):
-        print("Created!")
-        CreateOnWaitingTask(self.func.child_func)
+    async def input_value(self, user_id: int=None, group_id: int=None):
+        logger.info("A OnWaiting Task has been successfully created.")
+        CreateOnWaitingTask(self.func.child_func, user_id, group_id)
