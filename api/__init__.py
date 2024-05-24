cb9a8a15bd4f93a715525df2909289f309bc1cd3
Xinre_L_awa <75836010+Xinre-L-awa@users.noreply.github.com>
2024-05-24 18:51:54 +0800

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,9 @@
 from .bot import Bot as Bot
 from .event import NoticeEvent as NoticeEvent
 from .event import MessageEvent as MessageEvent
+from .event import OnWaitingEvent as OnWaitingEvent
 from .event import GroupMessageEvent as GroupMessageEvent
 from .event import PrivateMessageEvent as PrivateMessageEvent
-from .event import OnWaitingEvent as OnWaitingEvent
 
 from .on import on_at as on_at
 from .on import custom as custom
@@ -14,15 +14,19 @@ from .on import on_command as on_command
 from .on import on_keyword as on_keyword
 from .on import on_startup as on_startup
 from .on import on_waiting as on_waiting
+from .on import on_scheduled as on_scheduled
 from .on import add_child_func as add_child_func
 from .on import get_func_pool as get_func_pool
 from .on import get_plugin_pool as get_plugin_pool
 from .on import get_notice_func_pool as get_notice_func_pool
 from .on import get_waiting_task_pool as get_waiting_task_pool
+from .on import get_scheduled_task_pool as get_scheduled_task_pool
 
 from .utils import At as At
+from .utils import shutdown as shutdown
 from .utils import run_func as run_func
 from .utils import set_device as set_device
+from .utils import to_shutdown as to_shutdown
 from .utils import ImageSegment as ImageSegment
 from .utils import AnalyseCQCode as AnalyseCQCode
 from .utils import await_run_func as await_run_func
